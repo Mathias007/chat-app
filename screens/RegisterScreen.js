9e7b91bd434c1006c0722ecd63817c69fe36166e
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-16 00:04:26 +0200

    build primary screens structure

---
+++ screens/RegisterScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function RegisterScreen() {
+    return (
+        <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+            <Text>Register Screen</Text>
+        </View>
+    );
+}
---
+++ screens/RoomsListScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from "react-native";
+
+export default function RoomsListScreen() {
+    return (
+        <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+            <Text>RoomsListScreen Screen</Text>
+        </View>
+    )
+}
