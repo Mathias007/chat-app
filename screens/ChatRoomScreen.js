aeda6d762151aa93e68fc261121a5717d6f7219a
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-16 16:41:41 +0200

    Build ChatRoomScreen components implementing react-native-gifted-chat lib

--- screens/ChatRoomScreen.js
+++ screens/ChatRoomScreen.js
@@ -1,12 +1,67 @@
-import React from 'react'
-import { View, Text } from "react-native";
+import React from "react";
+import { Platform } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
+import emojiUtils from "emoji-utils";
 
-export default function ChatRoomScreen() {
-    return (
-        <View
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-        >
-            <Text>ChatRoom Screen</Text>
-        </View>
-    )
+import Message from "./components/ChatRoom/Message";
+
+export default class ChatRoomScreen extends React.Component {
+    state = {
+        messages: [],
+    };
+
+    componentDidMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: "Hello developer!!!",
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: "React Native",
+                        avatar: "https://placeimg.com/140/140/any",
+                    },
+                },
+            ],
+        });
+    }
+
+    onSend(messages = []) {
+        this.setState((previousState) => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }));
+    }
+
+    renderMessage(props) {
+        const {
+            currentMessage: { text: currText },
+        } = props;
+
+        let messageTextStyle;
+
+        // Make "pure emoji" messages much bigger than plain text.
+        if (currText && emojiUtils.isPureEmojiString(currText)) {
+            messageTextStyle = {
+                fontSize: 28,
+                // Emoji get clipped if lineHeight isn't increased; make it consistent across platforms.
+                lineHeight: Platform.OS === "android" ? 34 : 30,
+            };
+        }
+
+        return <Message {...props} messageTextStyle={messageTextStyle} />;
+    }
+
+    render() {
+        return (
+            <GiftedChat
+                messages={this.state.messages}
+                onSend={(messages) => this.onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+                renderMessage={this.renderMessage}
+            />
+        );
+    }
 }
