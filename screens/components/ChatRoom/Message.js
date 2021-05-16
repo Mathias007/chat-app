c42654e3ee24b8fb02ab94ca5af406741c9b396f
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-16 16:41:15 +0200

    Create Message and Bubble components implementing react-native-gifted-chat lib

---
+++ screens/components/ChatRoom/Message.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import { Avatar, Day, utils } from "react-native-gifted-chat";
+import Bubble from "./Bubble";
+
+const { isSameUser, isSameDay } = utils;
+
+export default class Message extends React.Component {
+    getInnerComponentProps() {
+        const { containerStyle, ...props } = this.props;
+        return {
+            ...props,
+            position: "left",
+            isSameUser,
+            isSameDay,
+        };
+    }
+
+    renderDay() {
+        if (this.props.currentMessage.createdAt) {
+            const dayProps = this.getInnerComponentProps();
+            if (this.props.renderDay) {
+                return this.props.renderDay(dayProps);
+            }
+            return <Day {...dayProps} />;
+        }
+        return null;
+    }
+
+    renderBubble() {
+        const bubbleProps = this.getInnerComponentProps();
+        if (this.props.renderBubble) {
+            return this.props.renderBubble(bubbleProps);
+        }
+        return <Bubble {...bubbleProps} />;
+    }
+
+    renderAvatar() {
+        let extraStyle;
+        if (
+            isSameUser(this.props.currentMessage, this.props.previousMessage) &&
+            isSameDay(this.props.currentMessage, this.props.previousMessage)
+        ) {
+            extraStyle = { height: 0 };
+        }
+
+        const avatarProps = this.getInnerComponentProps();
+        return (
+            <Avatar
+                {...avatarProps}
+                imageStyle={{
+                    left: [
+                        styles.slackAvatar,
+                        avatarProps.imageStyle,
+                        extraStyle,
+                    ],
+                }}
+            />
+        );
+    }
+
+    render() {
+        const marginBottom = isSameUser(
+            this.props.currentMessage,
+            this.props.nextMessage
+        )
+            ? 2
+            : 10;
+
+        return (
+            <View>
+                {this.renderDay()}
+                <View
+                    style={[
+                        styles.container,
+                        { marginBottom },
+                        this.props.containerStyle,
+                    ]}
+                >
+                    {this.renderAvatar()}
+                    {this.renderBubble()}
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        alignItems: "flex-end",
+        justifyContent: "flex-start",
+        marginLeft: 8,
+        marginRight: 0,
+    },
+    slackAvatar: {
+        height: 40,
+        width: 40,
+        borderRadius: 3,
+    },
+});
