b743934ba01c043b430782ead2f5132916d426dd
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-17 23:08:52 +0200

    install and config RN svg and RN svg transformer

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,27 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+    const {
+        resolver: { sourceExts, assetExts },
+    } = await getDefaultConfig(__dirname);
+
+    return {
+        transformer: {
+            babelTransformerPath: require.resolve(
+                "react-native-svg-transformer"
+            ),
+            getTransformOptions: async () => ({
+                transform: {
+                    experimentalImportSupport: false,
+                    inlineRequires: false,
+                },
+            }),
+        },
+        resolver: {
+            assetExts: assetExts.filter((ext) => ext !== "svg"),
+            sourceExts: [...sourceExts, "svg"],
+        },
+    };
+})();
