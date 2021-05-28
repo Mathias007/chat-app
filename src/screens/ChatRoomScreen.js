cd80bdf6b3f25d6b263c944837dc35c032a23f85
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-29 00:31:21 +0200

    Reconstruct ChatRoomScreen using new functions from MessageContainer and InputToolbar.

--- src/screens/ChatRoomScreen.js
+++ src/screens/ChatRoomScreen.js
@@ -1,17 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { Platform } from "react-native";
-
+import React, { useState, useEffect } from "react";
+import { StyleSheet } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
-import emojiUtils from "emoji-utils";
-
-import { useMutation } from "@apollo/client";
 
 import ScreenHeader from "./components/Header/ScreenHeader";
-import Message from "./components/ChatRoom/Message";
+
+import {
+    renderBubble,
+    renderMessage,
+    renderMessageText,
+} from "./components/ChatRoom/MessageContainer";
+
+import {
+    renderInputToolbar,
+    renderComposer,
+    renderSend,
+} from "./components/ChatRoom/InputToolbar";
+
+import { useMutation } from "@apollo/client";
 
 import { POST_NEW_MESSAGE } from "../graphql/sendMessage.mutation";
 
 export default function ChatRoomScreen({ navigation, route }) {
+    const [text, setText] = useState("");
     const [messages, setMessages] = useState([]);
     const [roomID, setRoomID] = useState("");
 
@@ -37,7 +47,7 @@ export default function ChatRoomScreen({ navigation, route }) {
 
     const [sendMessage, { data }] = useMutation(POST_NEW_MESSAGE);
 
-    function onSend(newMessages = []) {
+    const onSend = (newMessages = []) => {
         setMessages(GiftedChat.append(newMessages, messages));
 
         newMessages.forEach((message) => {
@@ -53,26 +63,15 @@ export default function ChatRoomScreen({ navigation, route }) {
                 },
             });
         });
-    }
+    };
 
-    function renderMessage(props) {
-        const {
-            currentMessage: { text: currText },
-        } = props;
-
-        let messageTextStyle;
-
-        // Make "pure emoji" messages much bigger than plain text.
-        if (currText && emojiUtils.isPureEmojiString(currText)) {
-            messageTextStyle = {
-                fontSize: 28,
-                // Emoji get clipped if lineHeight isn't increased; make it consistent across platforms.
-                lineHeight: Platform.OS === "android" ? 34 : 30,
-            };
-        }
-
-        return <Message {...props} messageTextStyle={messageTextStyle} />;
-    }
+    const parseLinkPattern = (linkStyle) => [
+        {
+            pattern: /#(\w+)/,
+            style: linkStyle,
+            onPress: (tag) => console.log(`Pressed on hashtag: ${tag}`),
+        },
+    ];
 
     const { name, roomPic } = route.params.chatData.room;
 
@@ -88,12 +87,33 @@ export default function ChatRoomScreen({ navigation, route }) {
             <GiftedChat
                 inverted={false}
                 messages={messages}
-                onSend={(messages) => onSend(messages)}
+                text={text}
+                onInputTextChanged={setText}
+                onSend={onSend}
                 user={{
                     _id: 1,
                 }}
+                alignTop
+                alwaysShowSend
+                scrollToBottom
+                renderInputToolbar={renderInputToolbar}
+                renderComposer={renderComposer}
+                renderSend={renderSend}
+                renderAvatar={null}
+                renderBubble={renderBubble}
                 renderMessage={renderMessage}
+                renderMessageText={renderMessageText}
+                isCustomViewBottom
+                messagesContainerStyle={styles.chatContainer}
+                parsePatterns={parseLinkPattern}
             />
         </>
     );
 }
+
+const styles = StyleSheet.create({
+    chatContainer: {
+        marginBottom: 12,
+        backgroundColor: "#F0F8FF",
+    },
+});
